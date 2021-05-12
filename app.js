5e9e5b27aff2790df18e4fd1fd825939366a37ac
Aniket <aniketrandhave15@gmail.com>
2021-05-12 13:28:04 +0530

    add route

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ app.use(cookieParser());
  
 app.use(require('./router/router'))  //use all router(endpoint) of page here by this code
 
+ 
+
 // horuku code
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"))
+    const path = require('path');
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(PORT, () => {
